2979951fb0e47f083a3e355cf2188767f6bc223e
Mikola Lysenko <mikolalysenko@gmail.com>
2014-05-28 10:26:46 -0500

    test case tweaks

--- test/test.js
+++ test/test.js
@@ -23,57 +23,62 @@ require("tape")("typedarray-pool", function(t) {
   for(var i=1; i<100; ++i) {
     var a
     a = pool.malloc(i, "int8")
-    t.assert(a instanceof Int8Array)
-    t.assert(a.length >= i)
+    t.assert(a instanceof Int8Array, "int8array valid")
+    t.assert(a.length >= i, "int8array length")
     pool.free(a)
     
     a = pool.malloc(i, "int16")
-    t.assert(a instanceof Int16Array)
+    t.assert(a instanceof Int16Array, "int16")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "int32")
-    t.assert(a instanceof Int32Array)
+    t.assert(a instanceof Int32Array, "int32")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "uint8")
-    t.assert(a instanceof Uint8Array)
+    t.assert(a instanceof Uint8Array, "uint8")
+    t.assert(!Buffer.isBuffer(a), "not buffer")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "uint16")
-    t.assert(a instanceof Uint16Array)
+    t.assert(a instanceof Uint16Array, "uint16")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "uint32")
-    t.assert(a instanceof Uint32Array)
+    t.assert(a instanceof Uint32Array, "uint32")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "float")
-    t.assert(a instanceof Float32Array)
+    t.assert(a instanceof Float32Array, "float")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i, "double")
-    t.assert(a instanceof Float64Array)
+    t.assert(a instanceof Float64Array, "double")
     t.assert(a.length >= i)
     pool.free(a)
 
     a = pool.malloc(i, "uint8_clamped")
-    t.assert(a instanceof Uint8ClampedArray)
+    if((typeof Uint8ClampedArray) !== "undefined") {
+        t.assert(a instanceof Uint8ClampedArray, "uint8_clamped")
+    } else {
+        t.assert(a instanceof Uint8Array, "unit8_clamped clamped default to uint8")
+    }
     t.assert(a.length >= i)
     pool.free(a)
 
     a = pool.malloc(i, "buffer")
-    t.assert(a instanceof Buffer)
+    t.assert(Buffer.isBuffer(a), "buffer")
     t.assert(a.length >= i)
     pool.free(a)
     
     a = pool.malloc(i)
-    t.assert(a instanceof ArrayBuffer)
+    t.assert(a instanceof ArrayBuffer, "array buffer")
     t.assert(a.byteLength >= i)
     pool.free(a)
   }
@@ -81,57 +86,62 @@ require("tape")("typedarray-pool", function(t) {
   for(var i=1; i<100; ++i) {
     var a
     a = pool.mallocInt8(i)
-    t.assert(a instanceof Int8Array)
+    t.assert(a instanceof Int8Array, "int8")
     t.assert(a.length >= i)
     pool.freeInt8(a)
     
     a = pool.mallocInt16(i)
-    t.assert(a instanceof Int16Array)
+    t.assert(a instanceof Int16Array, "int16")
     t.assert(a.length >= i)
     pool.freeInt16(a)
     
     a = pool.mallocInt32(i)
-    t.assert(a instanceof Int32Array)
+    t.assert(a instanceof Int32Array, "int32")
     t.assert(a.length >= i)
     pool.freeInt32(a)
     
     a = pool.mallocUint8(i)
-    t.assert(a instanceof Uint8Array)
+    t.assert(a instanceof Uint8Array, "uint8")
+    t.assert(!Buffer.isBuffer(a), "not buffer")
     t.assert(a.length >= i)
     pool.freeUint8(a)
     
     a = pool.mallocUint16(i)
-    t.assert(a instanceof Uint16Array)
+    t.assert(a instanceof Uint16Array, "uint16")
     t.assert(a.length >= i)
     pool.freeUint16(a)
     
     a = pool.mallocUint32(i)
-    t.assert(a instanceof Uint32Array)
+    t.assert(a instanceof Uint32Array, "uint32")
     t.assert(a.length >= i)
     pool.freeUint32(a)
     
     a = pool.mallocFloat(i)
-    t.assert(a instanceof Float32Array)
+    t.assert(a instanceof Float32Array, "float32")
     t.assert(a.length >= i)
     pool.freeFloat(a)
     
     a = pool.mallocDouble(i)
-    t.assert(a instanceof Float64Array)
+    t.assert(a instanceof Float64Array, "float64")
     t.assert(a.length >= i)
     pool.freeDouble(a)
     
     a = pool.mallocUint8Clamped(i)
-    t.assert(a instanceof Uint8ClampedArray)
+    if((typeof Uint8ClampedArray) !== "undefined") {
+        t.assert(a instanceof Uint8ClampedArray, "uint8 clamped")
+    } else {
+        t.assert(a instanceof Uint8Array, "uint8 clamped defaults to unt8")
+    }
     t.assert(a.length >= i)
     pool.freeUint8Clamped(a)
 
     a = pool.mallocBuffer(i)
-    t.assert(Buffer.isBuffer(a))
+    t.assert(Buffer.isBuffer(a), "buffer")
     t.assert(a.length >= i)
     pool.freeBuffer(a)
     
     a = pool.mallocArrayBuffer(i)
-    t.assert(a instanceof ArrayBuffer)
+    t.assert(a instanceof ArrayBuffer, "array buffer")
     t.assert(a.byteLength >= i)
     pool.freeArrayBuffer(a)
   }
